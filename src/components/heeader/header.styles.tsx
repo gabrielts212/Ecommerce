6c3b07f16a23d8391df24724fd89979b4c70eb8a
gabrielts212 <gabrielts2122@gmail.com>
2023-03-03 21:02:10 -0300

    loja

---
+++ src/components/heeader/header.styles.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import "./header.styles.css"
+import { BsCart3} from 'react-icons/bs';
+const header = () => {
+  return <div className="header-container">
+        <h2 className="header-title"> CLUB CLOTHING</h2>
+    
+            <div className="header-items">
+                <div className="header-item">Explorar</div>
+                <div className="header-item"> Login</div>
+                <div className="header-item">Criar Conta</div>
+                <div className="header-item">
+                    <BsCart3 size={25} />
+                <p style={{marginLeft:5}}>5</p>
+                </div>
+                
+        
+        
+    </div>
+  </div>
+}
+
+export default header
---
+++ src/components/pages/home/home.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../heeader/header.styles'
+const home = () => {
+  return (
+    <div>
+
+<Header/>
+
+    </div>
+  )
+}
+
+export default home
